6957e38352331e8efbcc4701b3189fb1a37c73f9
parvin <84388565+Parvin-Gasimli@users.noreply.github.com>
2021-11-16 23:52:11 +0300

    create new rander and if else

--- home/src/CartSumary.js
+++ home/src/CartSumary.js
@@ -4,31 +4,31 @@ import {
   DropdownToggle,
   DropdownMenu,
   DropdownItem,
-  Badge
+  Badge,
 } from "reactstrap";
 
 export default class CartSumary extends Component {
-  render() {
+  renderSummary() {
     return (
-      <div>
-        <UncontrolledDropdown inNavbar nav>
-          <DropdownToggle caret nav>
-            Your Cart-{this.props.cart.length}
-          </DropdownToggle>
-          <DropdownMenu right>
-            {this.props.cart.map((cartItem) => (
-              <DropdownItem key={cartItem.product.id}>
-                
-                {cartItem.product.productName}
-                <Badge color="success">{cartItem.count}</Badge>
-              </DropdownItem>
-            ))}
+      <UncontrolledDropdown inNavbar nav>
+        <DropdownToggle caret nav>
+          Your Cart-{this.props.cart.length}
+        </DropdownToggle>
+        <DropdownMenu right>
+          {this.props.cart.map((cartItem) => (
+            <DropdownItem key={cartItem.product.id}>
+              {cartItem.product.productName}
+              <Badge color="success">{cartItem.count}</Badge>
+            </DropdownItem>
+          ))}
 
-            <DropdownItem divider />
-            <DropdownItem>Reset</DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </div>
+          <DropdownItem divider />
+          <DropdownItem>Reset</DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
     );
+  }
+  render() {
+    return <div></div>;
   }
 }
